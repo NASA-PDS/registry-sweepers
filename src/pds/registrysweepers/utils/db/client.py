045a8d05f98c6ec90989403cb222c69c2d4b5df2
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-05-02 14:27:40 -0700

    implement access tests during opensearch client instantiation

--- src/pds/registrysweepers/utils/db/client.py
+++ src/pds/registrysweepers/utils/db/client.py
@@ -1,8 +1,12 @@
 import json
+import logging
 import os
 from typing import Union
 
+import requests
 from opensearchpy.client import OpenSearch
+
+log = logging.getLogger()
 
 
 def get_opensearch_client_from_environment(verify_certs: bool = True) -> OpenSearch:
@@ -23,6 +27,12 @@ def get_opensearch_client_from_environment(verify_certs: bool = True) -> OpenSea
 def get_opensearch_client(
     endpoint_url: str, username: Union[str, None] = None, password: Union[str, None] = None, verify_certs: bool = True
 ) -> OpenSearch:
+    if not (username is None) == (password is None):
+        raise ValueError(f"must provide both username and password, or neither")
+
+    credentials_supplied = username is not None
+    auth = (username, password) if credentials_supplied else None
+
     try:
         scheme, host, port_str = endpoint_url.replace("://", ":", 1).split(":")
         port = int(port_str)
@@ -32,8 +42,39 @@ def get_opensearch_client(
         )
 
     use_ssl = scheme.lower() == "https"
-    auth = (username, password) if username is not None else None
 
-    return OpenSearch(
+    test_url = f"{endpoint_url}/_cat/indices"
+    try:
+        log.info(
+            f'Testing access to OpenSearch endpoint {test_url} with{"out" if not credentials_supplied else ""} user/pass credentials...'
+        )
+        resp = requests.get(test_url, auth=auth)
+        resp.raise_for_status()
+        log.info(f"Access to {test_url} confirmed!")
+    except requests.HTTPError as err:
+        log.error(f"Request to {test_url} failed with {err}")
+        raise err
+
+    client = OpenSearch(
         hosts=[{"host": host, "port": int(port)}], http_auth=auth, use_ssl=use_ssl, verify_certs=verify_certs
     )
+
+    log.info("Testing OpenSearch client with ping...")
+    ping_response = client.ping()
+    if ping_response:
+        log.info(f"OpenSearch client ping test succeeded")
+    else:
+        msg = f"OpenSearch client ping test failed"
+        log.error(msg)
+        raise RuntimeError(msg)
+
+    log.info("Testing OpenSearch client with index list operation...")
+    try:
+        client.indices.get("*")
+        log.info(f"OpenSearch client index list test succeeded")
+    except Exception as err:
+        msg = f"OpenSearch client index list test failed with {err}"
+        log.error(msg)
+        raise RuntimeError(msg)
+
+    return client
