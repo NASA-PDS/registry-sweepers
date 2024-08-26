4aec699f90622e3425de87030634afa26dee2c30
Sagar Joshi <sagar.joshi@jpl.nasa.gov>
2024-08-26 12:51:59 -0700

    change account number for mcp

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,31 @@
 variable "aws_account_id" {
   description = "AWS Account Id"
-  default = "445837347542"
+  default     = "456238491188"
 }
 
 variable "node_name_abbr" {
   description = "Node name abbreviation"
-  default = "en"
+  default     = "en"
 }
 
 variable "venue" {
   description = "Deployment venue (prod, test, dev, delta, gamma, etc.)"
-  default = "dev"
+  default     = "dev"
 }
 
 variable "aws_region" {
   description = "AWS Region"
-  default = "us-west-2"
+  default     = "us-west-2"
 }
 
 variable "aws_shared_credentials_file" {
   description = "AWS shared credentials file"
-  default = "~/.aws/credentials"
+  default     = "~/.aws/credentials"
 }
 
 variable "aws_profile" {
   description = "AWS profile"
-  default = "default"
+  default     = "default"
 }
 
 variable "aws_fg_vpc" {
@@ -34,12 +34,12 @@ variable "aws_fg_vpc" {
 
 variable "aws_fg_security_groups" {
   description = "AWS Security groups for Fargate"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "aws_fg_subnets" {
   description = "AWS Subnets for Fargate"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "prov_endpoint" {
@@ -52,7 +52,7 @@ variable "prov_remote_lists" {
 
 variable "aws_ecr_repository" {
   description = "Name of the AWS ECR Repository"
-  default = "pds-registry-api-service"
+  default     = "pds-registry-api-service"
 }
 
 variable "aws_fg_image" {
@@ -61,7 +61,7 @@ variable "aws_fg_image" {
 
 variable "aws_ecr_pull_policy_arn" {
   description = "ARN for the ECR pull policy"
-  default = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+  default     = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
 variable "aws_eventbridge_role_arn" {
