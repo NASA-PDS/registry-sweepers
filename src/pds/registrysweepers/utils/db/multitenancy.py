7f55fa8ceb695c6687767b6947d040bae6db472a
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-01-29 11:11:20 -0800

    implement index alias resolution utility functions

--- src/pds/registrysweepers/utils/db/multitenancy.py
+++ src/pds/registrysweepers/utils/db/multitenancy.py
@@ -1,4 +1,7 @@
+import logging
 import os
+
+from opensearchpy import OpenSearch
 
 
 def resolve_multitenant_index_name(index_type: str):
@@ -11,3 +14,20 @@ def resolve_multitenant_index_name(index_type: str):
         raise ValueError(f'index_type "{index_type}" not supported (expected one of {supported_index_types})')
     else:
         return f"{node_id}-{index_type}"
+
+
+def index_exists(client: OpenSearch, index_or_alias_name: str) -> bool:
+    # counterintuitively, indices.exists does not return False if its argument is an alias.
+    # Possibly a bug in opensearch-py: https://github.com/opensearch-project/opensearch-py/issues/888
+    return client.indices.exists(index_or_alias_name) and not client.indices.exists_alias(index_or_alias_name)
+
+
+def resolve_index_name_if_aliased(client: OpenSearch, index_or_alias_name: str) -> str:
+    if index_exists(client, index_or_alias_name):
+        return index_or_alias_name
+    elif client.indices.exists_alias(index_or_alias_name):
+        index_name = next(iter(client.indices.get_alias('index_or_alias_name').keys()))
+        logging.debug(f'Resolved alias {index_or_alias_name} to index {index_name}')
+        return index_name
+    else:
+        raise ValueError(f'Could not resolve index for index_or_alias_name "{index_or_alias_name}"')
