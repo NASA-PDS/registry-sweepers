24a7ab9a213cd1696191828bddaece66424a69fe
Sagar Joshi <sagar.joshi@jpl.nasa.gov>
2025-02-27 12:55:02 -0800

    comment out backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "pds-infra"
-    key    = "dev/pds_tf_infra.tfstate"
-    region = "us-west-2"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "pds-infra"
+#     key    = "dev/registry_sweeper_ecs.tfstate"
+#     region = "us-west-2"
+#   }
+# }
