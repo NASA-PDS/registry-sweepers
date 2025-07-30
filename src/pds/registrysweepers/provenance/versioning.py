b4d73d077721d056d18b394517c6f8552c466b9c
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-07-30 13:33:40 -0700

    replace existing provenance software version metadata key with a new one this allows the typing to be corrected across all nodes without reindexing

--- src/pds/registrysweepers/provenance/versioning.py
+++ src/pds/registrysweepers/provenance/versioning.py
@@ -4,4 +4,8 @@
 from pds.registrysweepers.utils.misc import get_sweeper_version_metadata_key
 
 SWEEPERS_PROVENANCE_VERSION = 2
-SWEEPERS_PROVENANCE_VERSION_METADATA_KEY = get_sweeper_version_metadata_key("provenance")
+# At some point, provenance version metadata attributes were indexed differently as integer/keyword (should be integer)
+# As a hotfix, a new key is being used - eventually it will be necessary to re-index all nodes under corrected mappings,
+# as mappings may not be changed - edunn 20250730
+SWEEPERS_PROVENANCE_VERSION_METADATA_KEY = get_sweeper_version_metadata_key("provenance_hotfixed")
+SWEEPERS_BROKEN_PROVENANCE_VERSION_METADATA_KEY = get_sweeper_version_metadata_key("provenance")
