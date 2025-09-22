ce24b63c12a07bd83646d203b35069751f3bb4bb
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-09-22 15:21:07 -0700

    bump ancestry version everything has been tested, now ready for manual production run

--- src/pds/registrysweepers/ancestry/versioning.py
+++ src/pds/registrysweepers/ancestry/versioning.py
@@ -3,5 +3,5 @@
 # previously-processed data
 from pds.registrysweepers.utils.misc import get_sweeper_version_metadata_key
 
-SWEEPERS_ANCESTRY_VERSION = 5
+SWEEPERS_ANCESTRY_VERSION = 6
 SWEEPERS_ANCESTRY_VERSION_METADATA_KEY = get_sweeper_version_metadata_key("ancestry")
