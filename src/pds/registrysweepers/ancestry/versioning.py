5cab20979163c37297bcced43e5ee8d4d6167c55
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-11-03 16:59:40 -0800

    add comment

--- src/pds/registrysweepers/ancestry/versioning.py
+++ src/pds/registrysweepers/ancestry/versioning.py
@@ -4,4 +4,7 @@
 from pds.registrysweepers.utils.misc import get_sweeper_version_metadata_key
 
 SWEEPERS_ANCESTRY_VERSION = 7
+
+# applicable to bundle and collection documents - indicates the version of the ancestry sweeper which last successfully
+# processed the document to add it to its direct descendant products
 SWEEPERS_ANCESTRY_VERSION_METADATA_KEY = get_sweeper_version_metadata_key("ancestry")
