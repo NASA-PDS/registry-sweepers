1e7d0e21cc50d775aff21cae5c2a4b49b0f0bd4c
Thomas Loubrieu <loubrieu@jpl.nasa.gov>
2026-07-13 14:45:29 -0700

    fix syntax

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -158,7 +158,7 @@ resource "aws_iam_policy" "mwaa_ecs_passrole_policy" {
 resource "aws_iam_role_policy_attachment" "mwaa_ecs_passrole" {
   count      = local.enable_mwaa_passrole ? 1 : 0
   role       = var.mwaa_execution_role_name
-  policy_arn = aws_iam_policy.mwaa_ecs_passrole_policy.arn
+  policy_arn = aws_iam_policy.mwaa_ecs_passrole_policy[0].arn
 }
 
 resource "aws_iam_policy" "opensearch_api_only_access" {
