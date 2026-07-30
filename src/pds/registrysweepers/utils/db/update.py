e089d40da17c57fa000549d0fd83149e66ad1da9
edunn <alexander.e.dunn@jpl.nasa.gov>
2026-07-30 12:26:08 -0700

    incorporate inline script new_items into Update

--- src/pds/registrysweepers/utils/db/update.py
+++ src/pds/registrysweepers/utils/db/update.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Dict
 from typing import Union
+from typing import Optional
+
 
 
 @dataclass
@@ -10,7 +12,9 @@ class Update:
     """Class representing an ES/OpenSearch database update to a single document"""
 
     id: str
-    content: Dict
+    content: Optional[Dict] = None
+    inline_script_content: Optional[str] = None
+    inline_script_new_items: Optional[Iterable] = None
 
     # used when it is necessary to instantiate these updates for flow-control purposes
     # for example, the provenance sweeper needs to trigger bulk write buffer flushes as it iterates, even if only a
@@ -21,8 +25,6 @@ class Update:
     # see: https://www.elastic.co/guide/en/elasticsearch/reference/7.17/optimistic-concurrency-control.html
     primary_term: Union[int, None] = None
     seq_no: Union[int, None] = None
-
-    inline_script_content: Union[None, str] = None
 
     def has_versioning_information(self) -> bool:
         has_primary_term = self.primary_term is not None
@@ -40,3 +42,6 @@ class Update:
 
         if self.content and self.inline_script_content:
             raise ValueError("only one of content or inline_script_content may be provided in a single update")
+
+        if self.inline_script_content and self.inline_script_new_items is None:
+            raise ValueError("inline_script_content is provided, inline_script_new_items must be provided (but may be empty)")
