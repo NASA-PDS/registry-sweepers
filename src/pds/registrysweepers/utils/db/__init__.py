c2558e8276f2c4d2eb9f980742a8e1c646c31f35
edunn <alexander.e.dunn@jpl.nasa.gov>
2026-07-30 12:45:08 -0700

    incorporate changes into updates_as_statements()

--- src/pds/registrysweepers/utils/db/__init__.py
+++ src/pds/registrysweepers/utils/db/__init__.py
@@ -379,8 +379,7 @@ def update_as_statements(update: Update, as_upsert: bool = False) -> Iterable[st
                 "source": update.inline_script_content,
                 "lang": "painless",
                 "params": {
-                    # TODO: pull this out of the generic update_as_statements() - it's ancestry-specific
-                    "new_items": update.content.get(ANCESTRY_REFS_METADATA_KEY, []),
+                    "new_items": list(update.inline_script_new_items),
                 },
             }
 
